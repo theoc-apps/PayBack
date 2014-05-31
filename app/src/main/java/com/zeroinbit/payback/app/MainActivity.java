230960e29581e2e40db27ef2d54ea138df6e4084
Theodor Ciausu <theoc.apps@gmail.com>
2014-05-31 13:49:41 -0400

    Removed "test"

--- app/src/main/java/com/zeroinbit/payback/app/MainActivity.java
+++ app/src/main/java/com/zeroinbit/payback/app/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 public class MainActivity extends Activity {
-//test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
